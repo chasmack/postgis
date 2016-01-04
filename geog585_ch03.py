3923402274c60824c0c10c3cf5a0e56a72762972
chasmack <chas.mack@asis.com>
2016-01-04 01:02:09 -0800

    cleanup of ch03

--- geog585_ch03.py
+++ geog585_ch03.py
@@ -1,18 +1,14 @@
-
 # Trim the Philadelphia Base Layers shapefiles to the clipFeature/city_limits geometry,
 # reproject the shapefile geometry from EPSG:4326 to EPSG:3857 and load resulting
-# layers into PostGIS tables in the geog585_ch03 schema.
-#
-# The roads.shp geometry will be converted to wkbMultiLine in the process.
+# layers into PostGIS tables in the geog585 schema.
 
-import ogr
-import osr
+import ogr, osr
 import os
-import glob
+from glob import glob
 
 ogr.UseExceptions()
 
-# PostGIS compatible geometry names.
+# PostGIS geometry type modifiers.
 def postgis_geom_type(type):
     return 'Geometry' if type == 0 else ogr.GeometryTypeToName(type).replace(' ', '')
 
@@ -22,9 +18,9 @@ src_srid = 4326
 dest_schema = 'geog585'
 dest_srid = 3857
 
-DSN = 'PG: host=localhost port=5432 dbname=postgis_scratch user=postgres password=pg'
-
 trim_shapefile = 'clipFeature/city_limits.shp'
+
+DSN = 'PG: host=localhost dbname=postgis_scratch user=postgres password=pg'
 
 # SRS transform to project geometry: 4326 -> 3857
 src_srs = osr.SpatialReference()
@@ -34,26 +30,28 @@ dest_srs.ImportFromEPSG(dest_srid)
 srsTransform = osr.CoordinateTransformation(src_srs, dest_srs)
 
 # Open a connection to PostgreSQL.
-destConn = ogr.Open(DSN)
 destDS = ogr.Open(DSN, 1)
 
 # Driver for reading source shapefiles.
 srcDriver = ogr.GetDriverByName('ESRI Shapefile')
+os.chdir(src_dir)
 
-# Get clipping geometry.
-trimDS = srcDriver.Open(os.path.join(src_dir, trim_shapefile), 0)
+# Get trim geometry.
+trimDS = srcDriver.Open(trim_shapefile, 0)
 trimLayer = trimDS.GetLayer()
 trimFeature = trimLayer.GetFeature(0)
 trimGeom = trimFeature.GetGeometryRef()
 
-for shapefile in glob.glob(os.path.join(src_dir, '*.shp')):
+for shapefile in glob('*.shp'):
+
     srcDS = srcDriver.Open(shapefile, 0)
     srcLayer = srcDS.GetLayer()
     srcLayer.SetSpatialFilter(trimGeom)
 
     layer_name = srcLayer.GetName()
-    geom_type = srcLayer.GetGeomType()
-    print('{0}: {1}'.format(layer_name, postgis_geom_type(geom_type)))
+    geom_type = postgis_geom_type(srcLayer.GetGeomType())
+    feature_count = srcLayer.GetFeatureCount()
+    print('{0}: {1} count = {2}'.format(layer_name, geom_type, feature_count))
 
     # Common layer format.
     qstr = """
@@ -64,8 +62,9 @@ for shapefile in glob.glob(os.path.join(src_dir, '*.shp')):
       name character varying(48),
       type character varying(48),
       geom geometry({geom_type}, {srid}),
-      CONSTRAINT {layer}_pkey PRIMARY KEY (gid)) WITH (OIDS=FALSE);
-    """.format(schema=dest_schema, layer=layer_name, geom_type=postgis_geom_type(geom_type), srid=dest_srid)
+      CONSTRAINT {layer}_pkey PRIMARY KEY (gid))
+    WITH (OIDS=FALSE);
+    """.format(schema=dest_schema, layer=layer_name, geom_type=geom_type, srid=dest_srid)
 
     # Special processing for specific layers.
     if layer_name == 'roads':
@@ -96,46 +95,37 @@ for shapefile in glob.glob(os.path.join(src_dir, '*.shp')):
     for srcFeature in srcLayer:
         destFeature = ogr.Feature(destFeatureDefn)
 
-        for field in ['osm_id', 'name', 'type']:
-            destFeature.SetField(field,
-                srcFeature.GetField(srcFeature.GetFieldIndex(field)))
+        # Common fields for all layers.
+        field_names = ['osm_id', 'name', 'type']
+
+        # Extra fields for specific layers.
+        if layer_name == 'roads':
+            field_names += ['ref', 'oneway', 'bridge']
+
+        elif layer_name == 'waterways':
+            field_names += ['width']
+
+        elif layer_name == 'places':
+            field_names += ['population']
+
+        for field in field_names:
+            destFeature.SetField(field, srcFeature.GetField(field))
 
         geom = srcFeature.GetGeometryRef()
         geom.Transform(srsTransform)
 
-        # Extras for specific layers
+        # Convert road geometry to MultiLineString.
         if layer_name == 'roads':
-            for field in ['ref', 'oneway', 'bridge']:
-                destFeature.SetField(field, srcFeature.GetField(srcFeature.GetFieldIndex(field)))
-
-            # Convert line strings to multi line strings.
             geom = ogr.ForceToMultiLineString(geom)
-
-        elif layer_name == 'waterways':
-            for field in ['width']:
-                destFeature.SetField(field, srcFeature.GetField(srcFeature.GetFieldIndex(field)))
-
-        elif layer_name == 'places':
-            for field in ['population']:
-                destFeature.SetField(field,  srcFeature.GetField(srcFeature.GetFieldIndex(field)))
 
         destFeature.SetGeometry(geom)
         destLayer.CreateFeature(destFeature)
 
-        srcFeature.Destroy()
-        destFeature.Destroy()
-
-    srcDS.Destroy()
-
-# Change roads columns 'oneway' and 'bridge' to boolean.
+# Change road columns 'oneway' and 'bridge' to boolean.
 qstr = """
 ALTER TABLE {schema}.roads
-ALTER oneway TYPE boolean USING oneway::boolean,
-ALTER bridge TYPE boolean USING bridge::boolean;
+  ALTER oneway TYPE boolean USING oneway::boolean,
+  ALTER bridge TYPE boolean USING bridge::boolean;
 """.format(schema=dest_schema)
 destDS.ExecuteSQL(qstr)
-
-trimFeature.Destroy()
-trimDS.Destroy()
-
-destDS.Destroy()
+destDS = None
